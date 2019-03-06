2b5f243f34de3337476099b1eff019196f5350b4
eayllon1 <eayllon1@gmail.com>
2019-03-05 21:39:12 -0500

    added addressbook contract

--- eos/addressbook/addressbook.cpp
+++ eos/addressbook/addressbook.cpp
@@ -5,6 +5,54 @@ using std::string;
 
 class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
   public:
+    using contract::contract;
+
+    addressbook(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds) {}
+
+    [[eosio::action]]
+    void upsert(
+      name user,
+      string first_name,
+      string last_name,
+      string street,
+      string city,
+      string state
+    ) {
+      require_auth( user );
+      address_index addresses(_code, _code.value);
+      auto iterator = addresses.find(user.value);
+      if( iterator == addresses.end() )
+      {
+        addresses.emplace(user, [&]( auto& row ) {
+         row.key = user;
+         row.first_name = first_name;
+         row.last_name = last_name;
+         row.street = street;
+         row.city = city;
+         row.state = state;
+        });
+      }
+      else {
+        string changes;
+        addresses.modify(iterator, user, [&]( auto& row ) {
+          row.key = user;
+          row.first_name = first_name;
+          row.last_name = last_name;
+          row.street = street;
+          row.city = city;
+          row.state = state;
+        });
+      }
+    }
+
+    [[eosio::action]]
+    void erase(name user) {
+      require_auth(user);
+      address_index addresses(_self, _code.value);
+      auto iterator = addresses.find(user.value);
+      eosio_assert(iterator != addresses.end(), "Record does not exist");
+      addresses.erase(iterator);
+    }
 
   private:
     struct person {
@@ -20,5 +68,6 @@ class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
     }
 
     typedef eosio::multi_index<"people"_n, person> address_index;
-
 };
+
+EOSIO_DISPATCH( addressbook, (upsert)(erase))
